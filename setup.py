9fd2c5be7c6ce584a15daba0fc494831c1f941c4
i3visio <contacto@i3visio.com>
2014-07-06 03:34:08 +0200

    Usufy.py (Release): v1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(	name="Usufy",
-	version="1.0.0",
+	version="v1.0.0",
 	description="usufy.py - Piece of software to check the existence of a given profile in different platforms.",
 	author="Felix Brezo and Yaiza Rubio",
 	author_email="contacto@i3visio.com",
@@ -9,7 +9,7 @@ setup(	name="Usufy",
 	license="COPYING",
 	packages=["usufy", "usufy.wrappers"],
 	scripts=["bin/_template.py", "bin/classgenerator.py"],
-	long_description=open('README.md').read(),
+	long_description=open('README').read(),
 #	install_requires=[
 # 	"any >= 1.1.1",
 #	],
